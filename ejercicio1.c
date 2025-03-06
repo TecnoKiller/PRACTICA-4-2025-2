0e2afe3299627e5139fe4877d1d83f45dc39a862
TecnoKiller <leodrik.unam@gmail.com>
2025-03-06 14:04:10 -0600

    ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,15 @@
+// Silva Zinzu Andrik Leonardo
+// Ejercicio 1
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int a = 3;
+  int b = 2;
+  if (a > b);
+  {
+    print("a es mayor \n");
+  }
+  return 0;
+}
